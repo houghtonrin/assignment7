e3581dd9bf443d6dd999607e62d5abb6be8e2b5b
houghtonrin <112127182+houghtonrin@users.noreply.github.com>
2024-10-14 10:19:33 -0600

    fixed rounding

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function calculate(){
     let ppd = parseInt(document.getElementById("dogSize").value);
     let day = parseInt(document.getElementById("numDay").value);
     let pretax = ppd *day;
-    let tax = pretax * 0.047;
-    let total = pretax + tax;
+    let tax = parseFloat(pretax * 0.047).toFixed(2);
+    let total = pretax + parseFloat(tax);
     document.getElementById("price").textContent = "Price: $" +  pretax;
     document.getElementById("tax").textContent = "Tax: $" +  tax;
     document.getElementById("total").textContent = "Total: $" +  total;
